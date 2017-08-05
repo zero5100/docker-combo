f28d7c4fe6baf1d83d917c3bee8b22ef30dbc3bb
zero5100 <akellerstrass@gmail.com>
2017-08-05 18:42:52 -0500

    Create an nginx client project

--- server-api/app.js
+++ server-api/app.js
@@ -1,7 +1,10 @@
 const express = require('express')
     , app = express();
 
-app.use("/", express.static("public"));
+// app.use("/", express.static("public"));
+app.get('/api', function(req, res) {
+  res.send('Hello World!');
+});
 
 const server = app.listen(3000, function() {
   console.log('node-project listening on port 3000!');
